b54da296831962d886cff15bc72ac3d5e9195448
monthe-juimo <pamela.monthej@gmail.com>
2024-01-31 19:32:39 +0100

    Ajout du scroll view

--- app/src/main/java/com/ensicaen/sepa_vue_2/ui/historique/HistoriqueFragment.java
+++ app/src/main/java/com/ensicaen/sepa_vue_2/ui/historique/HistoriqueFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,23 +12,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ensicaen.sepa_vue_2.R;
-import com.ensicaen.sepa_vue_2.databinding.FragmentHistoriqueBinding;
+
 
 public class HistoriqueFragment extends Fragment {
 
-    private FragmentHistoriqueBinding binding;
-    private HistoriqueViewModel historiqueViewModel;
-    private RecyclerView recyclerView;
-    private HistoriqueAdapter historiqueAdapter;
-
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        historiqueViewModel = new ViewModelProvider(this).get(HistoriqueViewModel.class);
+        HistoriqueViewModel historiqueViewModel = new ViewModelProvider(this).get(HistoriqueViewModel.class);
         View root = inflater.inflate(R.layout.fragment_historique, container, false);
 
-        recyclerView = root.findViewById(R.id.recyclerViewHistorique);
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerViewHistorique);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        historiqueAdapter = new HistoriqueAdapter();
+        // Ajout du Scrollbar vertical
+        recyclerView.setVerticalScrollBarEnabled(true);
+
+        HistoriqueAdapter historiqueAdapter = new HistoriqueAdapter();
         recyclerView.setAdapter(historiqueAdapter);
 
         historiqueViewModel.getHistoriqueVirements().observe(getViewLifecycleOwner(), historiqueAdapter::setVirements);
@@ -38,9 +35,5 @@ public class HistoriqueFragment extends Fragment {
     }
 
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+
 }
